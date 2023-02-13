98c701301b9b47f585c100966a3ee73e3fc713a9
salehe291abbasi <saleheabbasi313@gmail.com>
2023-02-14 00:06:22 +0330

    changes some thing

--- src/assets/salehe/index.js
+++ src/assets/salehe/index.js
@@ -1,1 +1,8 @@
 
+export {default as prof1} from "./p1.png"
+export {default as prof2} from "./p3.png"
+export {default as prof3} from "./p4.png"
+export {default as prof4} from "./p5.png"
+export {default as prof5} from "./p6.png"
+export {default as prof6} from "./p7.png"
+export {default as prof7} from "./p8.png"
